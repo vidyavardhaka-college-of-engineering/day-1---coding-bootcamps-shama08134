c503c74695db8bf252b9c92bc1163d99b58463b4
shama08134 <>
2021-05-24 12:15:16 +0000

    printf

--- Program_1.c
+++ Program_1.c
@@ -1,1 +1,7 @@
-//Write a C Program to Display your Name
+#include<stdio.h>
+  int main()
+  {
+  printf("Hello Github\n");
+  printf("I am Shama\n");
+  return 0;
+  }//Write a C Program to Display your Name
